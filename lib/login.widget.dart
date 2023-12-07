593b78bf2f4d5c8b82872fdc0ec9feb1db2d10bf
MrPinut <50791972+MrPinut@users.noreply.github.com>
2023-12-07 09:46:33 +0100

    fiirst push

---
+++ lib/login.widget.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({super.key, required this.title});
+  final String title;
+
+  @override
+  State<LoginWidget> createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            const Text("congrat"),
+            SizedBox(
+              width: 300,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'UserID',
+                        ),
+                        onChanged: (str) {
+                          print("$str");
+                        }),
+                    TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                        ),
+                        onChanged: (str) {
+                          print("$str");
+                        }),
+                    TextField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'PassWord',
+                        ),
+                        onChanged: (str) {
+                          print("$str");
+                        }),
+                  ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
